7077a1879017d5f7c8a44df3a62410991d2415ab
Gregory Koberger <gkoberger@gmail.com>
2015-11-07 13:00:05 -0800

    Switch to using env variables

--- README.md
+++ README.md
@@ -1,86 +1,75 @@
-What Is It
-----------
+# What Is It
 
 Lets you set up a Slack both that's connected to Magic (getmagicnow.com). Basically, a personal consiegier right inside Slack.
 
 At [ReadMe.io](http://readme.io), our mascot Owlbert now takes care of all our needs! (Hence the name `owlbot`!)
 
-Installation
-------------
+# Installation
 
-**EASY SETUP**: Use [readmeio/owlbot-example](https://github.com/readmeio/owlbot-example)
+## Deploy to Heroku
 
-Set up a new folder + git repo:
+[ Insert link here ]
 
-    mkdir mybot && cd mybot
-    npm init
-    npm install owlbot --save
+*Note: You'll probably want to switch to the Hobby plan so it doesn't sleep*
 
-Create an index.js file, and edit it:
+## Configure
 
-    var owlbot = require('owlbot');
-    owlbot.configure({
-      
-      // The channel you want your bot to respond to requests in
-      // (Your bot will pass everything said in the channel to Magic)
-      channel: "#test-bot-channel",
+Set up the following variables:
 
-      // SLACK
-      //
-      // On your Integrations page in the Slack settings, do the following:
-      // 1. Click the "Bots" integration (Not "Slackbot"; that's different)
-      // 2. Fill out its username, and paste the API Token below
-      slack: {
-        token: "xoxb-###############-###########",
-      },
+### Slack Setup
 
-      // TWILIO
-      //
-      // 1. Buy a number here: https://www.twilio.com/user/account/phone-numbers/search
-      // 2. Copy the phone number and paste it below (format is "+1##########")
-      // 3. Set your SMS & MMS "Request URL" to the server this will be hosted on
-      // 4. Get your accountSid and authToken from here: https://www.twilio.com/user/account/settings
-      twilio: {
-        phoneNumber: "+1##########",
-        accountSid: "ABCDEFG",
-        authToken: "1234567",
-      },
+Owlbot can only work in one channel. Set the channel:
 
-      // By default, we check if the Twilio request is real (and not someone messing with you)
-      // If things aren't working for some reason, though, turn this to true.
-      bypassTwilioValidate: false,
+    heroku config:set OWLBOT_CHANNEL=#channelname
 
-      // The phone number for Magic (format: "+1##########")
-      magic: "+14082171721",
+Next, we have to get Owlbot into your Slack channel. On your Integrations page in the Slack settings, do the following:
 
-      // The port you want to run this on (the process.env.PORT stuff makes it work on Heroku)
-      port: process.env.PORT || 3012,
-    });
+    1. Click the "Bots" integration (Not "Slackbot"; that's different)
+    2. Fill out its username, and use the API Token ("xoxb-###############-###########") below:
 
-    owlbot.start();
+    heroku config:set OWLBOT_SLACK="xoxb-###############-###########"
 
-That's it! Once you have your bot configured, you can just say stuff in the channel and it will be forwarded to Magic.
+### Twilio Setup
 
-How It Works
-------------
+    1. Buy a number here: https://www.twilio.com/user/account/phone-numbers/search
+    2. Copy the phone number and paste it below (format is "+1##########")
+    3. Set your SMS & MMS "Request URL" to the server this will be hosted on
+    4. Get your accountSid and authToken from here: https://www.twilio.com/user/account/settings
+
+Set up the following:
+
+    heroku config:set OWLBOT_TWILIO_NUMBER="+1##########"
+    heroku config:set OWLBOT_TWILIO_ACCOUNTSID="ABCDEFG"
+    heroku config:set OWLBOT_TWILIO_AUTHTOKEN="1234567"
+
+
+By default, we check if the Twilio request is real (and not someone messing with you). If things aren't working for some reason, though, turn this to true.
+
+    [coming soon]
+
+### Set Up Magic
+
+The last setp is seting up the number for Magic. **NOTE:** We recommend you use your own phone number first to test everything out. Switch it to Magic's number when you're confident it's working!
+
+    heroku config:set OWLBOT_MAGIC="+1##########"
+
+When you're ready, the Magic number is `"+14082171721"`.
+
+### Message Magic
+
+That's it! Once you have your bot configured, send a message in the chat and Magic will get you set up!
+
+# How It Works
 
 When you pick a channel, anything said in the channel will be directed to Magic. Don't say anything in the channel that you don't want to go to Magic!
 
-You may want to use your own personal phone number for the "magic" option in config.js until everything's configured.
+You may want to use your own personal phone number for the "magic" option in the config until everything's configured.
 
 When you have it all set up, just say "Hello" in the channel, and you'll get a welcome message from Magic.
 
-Other Notes
------------
+# Other Notes
 
 This can both send and recieve images, so no need to worry about that!
 
 It currently limits messages to 160 characters; I'm honestly not sure what would happen on Magic's side without the limit. If it turns out they can indeed get longer messages, I'll remove it.
-
-Deploying to Heroku
--------------------
-
-Unfortunately, you'll need to deploy it publicly so Twilio can contact it.
-
-You should be able to deploy to Heroku easily. You'll need to remove `config.js` from `.gitignore`, and check it in for things to work.
 
--- index.js
+++ index.js
@@ -1,198 +1,41 @@
-var express = require('express')
-  , Slack = require('slack-client')
-  , Twilio = require('twilio')
+var owlbot = require('./lib');
+owlbot.configure({
+  
+  // The channel you want your bot to respond to requests in
+  // (Your bot will pass everything said in the channel to Magic)
+  channel: process.env.OWLBOT_CHANNEL,
 
-module.exports = {
-
-  // The user-specified options (set via this.configure)
-  opts: {},
-
-  // These are our "global" variables
-  channel: undefined,
-  slack: undefined,
-  twilio: undefined,
-
-  // Set options
-  configure: function(opts) {
-    this.opts = opts;
+  // SLACK
+  //
+  // On your Integrations page in the Slack settings, do the following:
+  // 1. Click the "Bots" integration (Not "Slackbot"; that's different)
+  // 2. Fill out its username, and paste the API Token below
+  // "xoxb-###############-###########"
+  slack: {
+    token: process.env.OWLBOT_SLACK,
   },
 
-  start: function() {
-    var opts = this.opts;
-    var self = this;
-
-    // If the channel doesn't start with a #, add it.
-    if(this.opts.channel.indexOf('#') != 0) {
-      this.opts.channel = "#" + this.opts.channel;
-    }
-
-    // Set up Slack
-    this.slack = new Slack(this.opts.slack.token, true, true);
-
-    // Set up Twilio
-    this.twilio = Twilio(this.opts.twilio.accountSid, this.opts.twilio.authToken);
-
-    // Listen for slack connection
-    this.slack.on('open', function() {
-      var channel, id, messages;
-      var channels = (function() {
-        var ref, results;
-        ref = self.slack.channels;
-        results = [];
-        for (id in ref) {
-          channel = ref[id];
-          if (channel.is_member) {
-            results.push("#" + channel.name);
-          }
-
-          if(channel.name == self.opts.channel.replace(/#/, '')) {
-            self.channel = channel;
-          }
-        }
-        return results;
-      })();
-
-      if(!self.channel) {
-        console.log("ERROR: Your bot isn't in the specified channel (" + self.opts.channel + ")!");
-        return;
-      }
-
-      console.log("Welcome to Slack. You are @" + self.slack.self.name + " of " + self.slack.team.name);
-      console.log('  - You are in: ' + channels.join(', '));
-    });
-
-    // Slack listen for messages
-    self.slack.on('message', function(message) {
-      var channel, channelError, channelName, errors, response, text, textError, ts, type, typeError, user, userName;
-      channel = self.slack.getChannelGroupOrDMByID(message.channel);
-      user = self.slack.getUserByID(message.user);
-      response = '';
-      type = message.type, ts = message.ts, text = message.text;
-      channelName = (channel != null ? channel.is_channel : void 0) ? '#' : '';
-      channelName = channelName + (channel ? channel.name : 'UNKNOWN_CHANNEL');
-      userName = (user != null ? user.name : void 0) != null ? "@" + user.name : "UNKNOWN_USER";
-
-      function clean(text) {
-        return text.replace(new RegExp("^" + userString + ":?\\s"), '');
-      }
-
-      if ((type === 'message') && (text != null) && (channel != null)) {
-
-        var userString = '<@' + self.slack.self.id + '>';
-
-        // Is it from the bot?
-        if(message.user == self.slack.self.id) {
-          return;
-        }
-
-        if(channelName == self.opts.channel && message.subtype == 'file_share') {
-          // This is an image upload! (We use timeouts so they hopefully show up in order)
-          self.handleSlackQuery(clean(message.file.title), channel);
-          setTimeout(function() {
-            self.handleSlackQuery(message.file.url, channel);
-          }, 2000)
-          if(message.file.initial_comment && message.file.initial_comment.comment) {
-            setTimeout(function() {
-              self.handleSlackQuery(clean(message.file.initial_comment.comment), channel);
-            }, 4000)
-          }
-          return;
-        } else if(channelName != self.opts.channel || message.subtype) {
-          // We only want to send something if it's in the right channel.
-          // (`message.subtype` indicates it's something like "John joined the channel", etc)
-          var isDirectMessage = channelName.indexOf('#') != 0;
-          var isRecipientOfMessage = text.indexOf('<@'+self.slack.self.id+'>') == 0;
-
-          if(isDirectMessage || isRecipientOfMessage) {
-            channel.send("Sorry, I can only respond in <#" + self.channel.id + ">!" );
-          }
-          return;
-        }
-
-        if(text.length > 160) {
-          channel.send("Hey chatterbox, keep it under 160 characters!");
-          return;
-        }
-
-        self.handleSlackQuery(clean(text), channel);
-      } else {
-        typeError = type !== 'message' ? "unexpected type " + type + "." : null;
-        textError = text == null ? 'text was undefined.' : null;
-        channelError = channel == null ? 'channel was undefined.' : null;
-        errors = [typeError, textError, channelError].filter(function(element) {
-          return element !== null;
-        }).join(' ');
-        return console.log("@" + self.slack.self.name + " could not respond. " + errors);
-      }
-    });
-
-    // Slack error
-    self.slack.on('error', function(error) {
-      return console.error("Error: " + error);
-    });
-
-    // Set up our express server
-    var app = express();
-    var bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded());
-    app.use(bodyParser.json());
-
-    // Wait for a post from Twilio
-    app.post('/', function (req, res) {
-      if (self.opts.bypassTwilioValidate || Twilio.validateExpressRequest(req, self.opts.twilio.authToken)) {
-        if(req.body.Body) {
-          var body = req.body.Body;
-
-          if(req.body.MediaUrl0) {
-            body += " " + req.body.MediaUrl0;
-          }
-
-          if(req.body.MediaUrl1) {
-            body += " " + req.body.MediaUrl1;
-          }
-
-          console.log('[RECEIVED]', body);
-          self.channel.send(body);
-        }
-        res.send("");
-      } else {
-        var msg = 'Not verified as being from Twilio! (You may want to turn on bypassTwilioValidate if you keep getting this.)';
-        console.log('Error: ' + msg);
-        res.send(msg);
-      }
-    });
-
-    // Run the express server
-    var server = app.listen(self.opts.port, function () {
-      var host = server.address().address;
-      var port = server.address().port;
-
-      console.log('Slack bot is listening at http://%s:%s', host, port);
-    });
-
-    // Start the Slack connection
-    self.slack.login();
+  // TWILIO
+  //
+  // 1. Buy a number here: https://www.twilio.com/user/account/phone-numbers/search
+  // 2. Copy the phone number and paste it below (format is "+1##########")
+  // 3. Set your SMS & MMS "Request URL" to the server this will be hosted on
+  // 4. Get your accountSid and authToken from here: https://www.twilio.com/user/account/settings
+  twilio: {
+    phoneNumber: process.env.OWLBOT_TWILIO_NUMBER,
+    accountSid: process.env.OWLBOT_TWILIO_ACCOUNTSID,
+    authToken: process.env.OWLBOT_TWILIO_AUTHTOKEN,
   },
 
-  // If we have a valid slack message to pass along
-  handleSlackQuery: function(query, channel) {
+  // By default, we check if the Twilio request is real (and not someone messing with you)
+  // If things aren't working for some reason, though, turn this to true.
+  bypassTwilioValidate: false,
 
-    // Don't send slack-specific emoji
-    query = query.replace(/:simple_smile:/, ':)');
-    query = query.replace(/:smile:/, ':D');
-    query = query.replace(/:smiley:/, ':)');
-    query = query.replace(/:wink:/, ';)');
-    query = query.replace(/:stuck_out_tongue:/, ':P');
-    query = query.replace(/:kiss:/, ':*');
+  // The phone number for Magic (format: "+1##########"), Magic is "+14082171721".
+  magic: process.env.OWLBOT_MAGIC,
 
-    console.log('[SENT]', query);
-    this.twilio.messages.create({
-      body: query,
-      to: this.opts.magic,
-      from: this.opts.twilio.phoneNumber,
-    }, function(err, message) {
-      if(err) console.log(err);
-    });
-  },
+  // The port you want to run this on (the process.env.PORT stuff makes it work on Heroku)
+  port: process.env.PORT || 3012,
+});
 
-};
+owlbot.start();
